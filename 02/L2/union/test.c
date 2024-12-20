d5879467918de791dfb83398fd64dce087c93df3
Lu Li <lli2@F2N045.42adel.org.au>
2024-12-20 19:07:37 +1030

    1

--- 02/L2/union/test.c
+++ 02/L2/union/test.c
@@ -8,6 +8,22 @@ int len(char *str) {
     return (i);
 }
 
+void	cmb(char *s1, char *s2, char *str){
+	
+	int	i =	0;
+
+	while(s1[i]){
+		str[i] = s1[i];
+		i++;
+	}
+	while(s2[i-len(s1)]){
+		str[i] = s2[i-len(s1)];
+		i++;
+	}
+	str[i] = '\0';
+
+}
+
 void smlwr(char *str) {
     int i;
     int j;
@@ -32,10 +48,16 @@ void smlwr(char *str) {
 }
 
 int main(int argc, char *argv[]) {
-    char *c1 = argv[1];
-    char *str;
-
-    smlwr(c1);
+    char   *s1 = argv[1];
+    char   *s2 = argv[2];
+	char	s[len(s1)+len(s2)+1];
+    
+    if(argc != 3){
+        write(1, "\n", 1);
+        return(0);
+    }
+    cmb(s1, s2, s);
+    smlwr(s);
     write(1, "\n", 1);
     return 0;
 }
